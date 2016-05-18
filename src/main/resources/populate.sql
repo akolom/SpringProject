7828f5aedff707f6a78344c80130f0c795c4284c
Sam <d.chiproeng@gmail.com>
2016-05-17 22:58:16 -0500

    update populate.sql

--- src/main/resources/populate.sql
+++ src/main/resources/populate.sql
@@ -1,6 +1,6 @@
-DELETE FROM `user`;
-DELETE FROM `usercredentials`;
+DELETE FROM `User`;
+DELETE FROM `UserCredentials`;
 
 
-INSERT INTO `usercredentials` VALUES ('quan','ROLE_USER','','$2a$10$0twQoQgdh8VYg4.vj1bBOe4sJqHazyl41ErAq/RN4ohLuHGLcKe9m'),('sammy','ROLE_ADMIN','','$2a$10$0twQoQgdh8VYg4.vj1bBOe4sJqHazyl41ErAq/RN4ohLuHGLcKe9m');
-INSERT INTO `user` VALUES (1,24,'Chi Proeng','M','Dov','sammy'),(3,29,'Quan','M','Dao','quan');
+INSERT INTO `UserCredentials` VALUES ('quan','ROLE_USER','','$2a$10$0twQoQgdh8VYg4.vj1bBOe4sJqHazyl41ErAq/RN4ohLuHGLcKe9m'),('sammy','ROLE_ADMIN','','$2a$10$0twQoQgdh8VYg4.vj1bBOe4sJqHazyl41ErAq/RN4ohLuHGLcKe9m');
+INSERT INTO `User` VALUES (1,24,'Chi Proeng','M','Dov','sammy'),(3,29,'Quan','M','Dao','quan');
